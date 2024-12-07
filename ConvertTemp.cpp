6b9f99a1b0b3cbd78f3bc3fbe8db23a5bbfb56f6
denul-e <danielmetry12@gmail.com>
2024-12-07 00:06:08 -0800

    Update ConvertTemp.cpp

--- ConvertTemp.cpp
+++ ConvertTemp.cpp
@@ -1,1 +1,9 @@
+#include <stdio.h>
 
+int main() {
+    double celsius = 100.0;  // You can change this value
+    double fahrenheit = (9.0 / 5.0) * celsius + 32.0;
+    printf("Celsius: %.2f\n", celsius);
+    printf("Fahrenheit: %.2f\n", fahrenheit);
+    return 0;
+}
