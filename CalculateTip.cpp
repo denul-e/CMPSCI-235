deb5659f0d1a5d219988368017cb5ab7261ffa21
denul-e <danielmetry12@gmail.com>
2024-12-07 00:06:53 -0800

    Update CalculateTip.cpp

--- CalculateTip.cpp
+++ CalculateTip.cpp
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+int main() {
+    double subtotal = 50.0;  // You can change this value
+    double tiprate = 0.15;   // You can change this value
+    double tip = subtotal * tiprate;
+    double total = subtotal + tip;
+    printf("Subtotal: %.2f\n", subtotal);
+    printf("Tip: %.2f\n", tip);
+    printf("Total: %.2f\n", total);
+    return 0;
+}
