aac1d2549b7879d247f4e0ee988e63d01ca8d165
denul-e <danielmetry12@gmail.com>
2024-12-07 00:25:18 -0800

    Update COC.cpp

--- COC.cpp
+++ COC.cpp
@@ -1,10 +1,1 @@
-#include <stdio.h>
 
-int main() {
-    // Printing the COC pattern
-    printf(" C   C     O   O        C   C\n");
-    printf("C         O     O     C\n");
-    printf("C         O     O     C\n");
-    printf(" C   C     O   O        C   C\n");
-    return 0;
-}
