877eab1cdd23cb9c1e16fdf47c0f93455045ae5b
denul-e <danielmetry12@gmail.com>
2024-12-07 00:17:27 -0800

    Update CalculateTip.cpp

--- CalculateTip.cpp
+++ CalculateTip.cpp
@@ -1,12 +1,14 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 int main() {
-    double subtotal = 50.0;  // You can change this value
-    double tiprate = 0.15;   // You can change this value
+    double subtotal = 50.0; // You can change this value
+    double tiprate = 0.15;  // 15% tip
     double tip = subtotal * tiprate;
     double total = subtotal + tip;
-    printf("Subtotal: %.2f\n", subtotal);
-    printf("Tip: %.2f\n", tip);
-    printf("Total: %.2f\n", total);
+
+    cout << "Subtotal: $" << subtotal << endl;
+    cout << "Tip: $" << tip << endl;
+    cout << "Total: $" << total << endl;
     return 0;
 }
