4d0581da934cf50c0007bcb6c78dbc3319f6abd6
denul-e <danielmetry12@gmail.com>
2024-12-07 00:10:10 -0800

    Update MyInitials.cpp

--- MyInitials.cpp
+++ MyInitials.cpp
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main() {
+    printf("       J\n");
+    printf("       J\n");
+    printf("       J\n");
+    printf("       J\n");
+    printf("       J\n");
+    printf("J      J\n");
+    printf("J      J\n");
+    printf(" J J J\n");
+    return 0;
+}
