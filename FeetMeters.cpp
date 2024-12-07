6bd37382a490776500b7df6eeb3718884a792df5
denul-e <danielmetry12@gmail.com>
2024-12-07 00:17:14 -0800

    Update FeetMeters.cpp

--- FeetMeters.cpp
+++ FeetMeters.cpp
@@ -1,8 +1,10 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 int main() {
-    double feet = 10.0;  // You can change this value
+    double feet = 10.0; // You can change this value
     double meters = feet * 0.305;
-    printf("%.2f feet is %.2f meters\n", feet, meters);
+
+    cout << feet << " feet is " << meters << " meters." << endl;
     return 0;
 }
