701b58c240c295c2d317a112cfbbb4e3626a45cb
denul-e <danielmetry12@gmail.com>
2024-12-07 00:06:24 -0800

    Update CalculateCylinder.cpp

--- CalculateCylinder.cpp
+++ CalculateCylinder.cpp
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+int main() {
+    double radius = 5.0;  // You can change this value
+    double length = 10.0; // You can change this value
+    double area = radius * radius * 3.14159;
+    double volume = area * length;
+    printf("Area of the cylinder: %.2f\n", area);
+    printf("Volume of the cylinder: %.2f\n", volume);
+    return 0;
+}
